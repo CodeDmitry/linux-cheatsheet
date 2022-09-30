edfef70629b7425a9db2c7aa7954b8a17d50d25a
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-09-30 10:54:53 -0400

    Update bashrc_colorls.sh

--- bashrc_colorls.sh
+++ bashrc_colorls.sh
@@ -1,3 +1,3 @@
-# | Append this line somewhere within ~/.bashrc to 
+# | Append this line somewhere within ~/.bashrc, or ~/.profile or whatever your default config file is for your shell. 
 # |     have colorful ls(eg directory color).
 alias ls='ls --color=auto' 
